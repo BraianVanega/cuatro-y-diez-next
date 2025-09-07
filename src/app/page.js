dc2ddbd25c1f0334b808a547a6a08dc23a1574d0
Eric1432 <ericbustamante@outlook.com>
2025-09-07 20:02:41 -0300

    Agregue el footer y la sección nosotros

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,16 @@
-import { Body, Header, MovingImages } from "@/components";
+import { Body, Header, MovingImages, Footer } from "@/components";
+import QuienesSomos from "@/components/QuienesSomos";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+    <div className="flex flex-col min-h-screen font-sans bg-blue-100">
       <Header />
       <main>
+      <MovingImages />
         <Body />
-        <MovingImages />
+        <QuienesSomos/>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center"></footer>
+      <Footer/>
     </div>
   );
 }
