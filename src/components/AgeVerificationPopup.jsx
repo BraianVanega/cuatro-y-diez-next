59bca097c40a26f08078fea95af1e6704cdb0e2d
Eric1432 <ericbustamante@outlook.com>
2025-11-06 20:10:54 -0300

    Agregue el boton de comprar por WhatsApp

--- src/components/AgeVerificationPopup.jsx
+++ src/components/AgeVerificationPopup.jsx
@@ -1,76 +1,61 @@
 "use client";
-import { motion } from "motion/react";
+
 import { useEffect, useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 
-export default function AgeVerificationPopup() {
-  const [isOpen, setIsOpen] = useState(true);
+export default function AgeVerificationPopup () {
+    const [isOpen, setIsOpen] = useState(false);
 
-  useEffect(() => {
-    const isVerified = localStorage.getItem("ageVerified");
-    if (!isVerified) {
-      setIsOpen(true);
-    }
-  }, []);
+    useEffect(() => {
+        const isVerified = localStorage.getItem("ageVerified");
+        if (!isVerified){
+            setIsOpen(true);
+        }
+    }, []);
 
-  useEffect(() => {
-    if (isOpen) {
-      document.body.style.overflow = "hidden";
-    } else {
-      document.body.style.overflow = "unset";
-    }
-
-    return () => {
-      document.body.style.overflow = "unset";
+    const handleConfirm = () => {
+        localStorage.setItem("ageVerified", "true");
+        setIsOpen(false);
     };
-  }, [isOpen]);
 
-  const handleConfirm = () => {
-    localStorage.setItem("ageVerified", "true");
-    setIsOpen(false);
-  };
+    const handleDeny = () =>{
+        window.location.href = "https://google.com";
+    };
 
-  const handleDeny = () => {
-    window.location.href = "https://google.com";
-  };
+    if (!isOpen) return null;
 
-  if (!isOpen) return null;
+    return (
+        <div className="fixed inset-0 bg-black/70 flex items-center justify-center z-50">
+            <div className="bg-white rounded-2xl p-8 shadow-xl text-center max-w-sm mx-auto">
+                <h2 className="text-2xl font-semibold mb-4 text-gray-800">
+                    ¿Sos mayor de 18 años?
+                    </h2>
+                    <p className="mb-6 text-gray-600">
+                        Debes tener al menos 18 años para ingresar a este sitio.
+                        </p>
+                        <div className="flex justify-center gap-4">
+                            <button 
+                            onClick={handleConfirm}
+                            className="bg-green-600 text-white px-5 py-2 rounded-lg hover:bg-green-700"
+                            >
+                                Si
+                            </button>
+                            <button
+                            onClick={handleDeny}
+                            className="bg-red-500 text-white px-5 py-2 rounded-lg hover:bg-red-600"
+                            >
+                                No
+                                </button>
+                        </div>
 
-  return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-      transition={{ duration: 0.5 }}
-      className="fixed w-full h-screen top-0 left-0 bg-black flex items-center justify-center z-50"
-    >
-      <motion.div
-        initial={{ opacity: 0, y: 100 }}
-        animate={{ opacity: 1, y: 0 }}
-        exit={{ opacity: 0, y: 100 }}
-        transition={{ duration: 0.5 }}
-        className="bg-white rounded-2xl p-8 shadow-xl text-center max-w-sm mx-auto"
-      >
-        <h2 className="text-2xl font-semibold mb-4 text-gray-800">
-          ¿Sos mayor de 18 años?
-        </h2>
-        <p className="mb-6 text-gray-600">
-          Debes tener al menos 18 años para ingresar a este sitio.
-        </p>
-        <div className="flex justify-center gap-4">
-          <button
-            onClick={handleConfirm}
-            className="bg-green-600 text-white px-5 py-2 rounded-lg hover:bg-green-700"
-          >
-            Si
-          </button>
-          <button
-            onClick={handleDeny}
-            className="bg-red-500 text-white px-5 py-2 rounded-lg hover:bg-red-600"
-          >
-            No
-          </button>
+            </div>
+
         </div>
-      </motion.div>
-    </motion.div>
-  );
+    );
+
+
+
+
+
 }
+
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,13 +1,21 @@
 import Image from "next/image";
+import { FaWhatsapp } from "react-icons/fa";
 
 export default function ProductCard({ img, name, description, price }) {
-  return (
-    <a
-      href="#"
-      className="bg-white shadow-md rounded-2xl overflow-hidden h-full cursor-pointer group transition-all duration-300 hover:shadow-[0_8px_30px_rgba(0,0,0,0.55)]"
-    >
 
-      <div className="relative w-full flex flex-col items-center h-full">
+  const phoneNumber = "5402234210160";
+
+  const message = `Hola, quiero consultar por este producto ${name}`;
+  const whatsappLink = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`
+
+  const formattedPrice = "$" + Number(price).toLocaleString("es-AR", {
+    minimumFractionDigits: 0,
+  });
+  
+  return (
+    
+      <div className="bg-white shadow-md rounded-2xl overflow-hidden h-full cursor-pointer group transition-all duration-300 hover:shadow-[0_8px_30px_rgba(0,0,0,0.55)]">
+        <div className="relative w-full flex flex-col items-center h-full">
         <figure className="overflow-hidden relative w-full h-[300px]">
           <Image
             src={img}
@@ -20,9 +28,22 @@ export default function ProductCard({ img, name, description, price }) {
         <div className="p-8 text-center flex flex-col gap-4">
           <h3 className="text-xl font-semibold text-blue-500">{name}</h3>
           <p className="text-gray-600">{description}</p>
-          <p className="text-black text-bold">${price}</p>
+          <p className="text-black text-bold">{formattedPrice}</p>
         </div>
-      </div>
-    </a>
+
+        {/* Boton de WhatsApp */}
+        <div className="pb-6">
+        <a
+        href={whatsappLink}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="flex items-center justify-center gap-2 bg-gradient-to-r from-green-500 to-emerald-600 text-white font-semibold px-6 py-2 rounded-full shadow-md hover:from-green-600 hover:to-emerald-700 transition-all">
+          <FaWhatsapp size={20} />
+          <span>Pedir por WhatsApp</span>
+          </a>
+          </div>
+          </div>
+          </div>
+      
   );
 }
