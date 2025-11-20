86943fea75d42cae4ffafe7e5dc6c5278096a6cb
Eric1432 <ericbustamante@outlook.com>
2025-11-20 19:42:02 -0300

    verificacion de google

---
+++ src/app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+    return [
+        {
+        url: "https://cuatro-y-10-next.vercel.app/",
+        lastModified: new Date(),
+        changeFrequency: "monthly",
+        priority: 1.0,
+    },
+    ];
+}
